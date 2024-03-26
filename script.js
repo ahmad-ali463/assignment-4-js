959361996bf2b662fde0e56597dd97b2e34167f5
Ahmad Ali <spyali463@gmail.com>
2024-03-26 20:49:35 +0500

    assignment

---
+++ script.js
@@ -0,0 +1,31 @@
+// document.getElementById("simpleAlert").onclick = function(){
+//     alert("I am in alert")
+// }
+function simpleAlert(){
+    alert("I am in alert")
+
+}
+
+
+function printMyName(){
+    let myname = document.getElementById("inputText").value;
+
+    if(myname === ""){
+        alert("please type your name")
+        return;
+    }
+    document.getElementById("output").innerHTML = myname
+}
+
+let cities = ['faisalabad','multan','karachi', 'lahore','peshawar','hasilpur']
+function printAllCities(){
+document.getElementById("output").innerHTML="";
+
+
+
+for ( let index = 0; index < 6; index++){
+ let city = cities[index]
+ console.log('city', city)
+ document.getElementById("output").innerHTML += city
+}
+}
